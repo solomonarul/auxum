5b17b318e3d7bcd5b00d20b35c495b902cb9de12
Solomonarul <solomonarul@tuta.io>
2025-03-12 20:42:26 +0200

    fix(cmake): Remove MSVC support, we only GNU.

--- inc/auxum/std.h
+++ inc/auxum/std.h
@@ -6,6 +6,10 @@
 #define _CRT_SECURE_NO_WARNINGS     // Windows fix for being annoying.
 #endif
 
+#ifdef BUILD_TYPE_VITA
+#define __psp2__                    // Vita fix for being slightly annoying.
+#endif
+
 #include "std/error.h"
 #include "std/unused.h"
 #include "std/strings.h"
