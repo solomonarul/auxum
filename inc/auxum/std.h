b8c39e2105af55f2634ae1ef6ca0e6dc3eb896d9
Solomonarul <solomonarul@tuta.io>
2025-03-15 13:22:02 +0200

    feat(Vita): Add PSVita heap limits.

--- inc/auxum/std.h
+++ inc/auxum/std.h
@@ -3,11 +3,13 @@
 #define AUXUM_STD_H
 
 #ifdef BUILD_TYPE_WINDOWS
-#define _CRT_SECURE_NO_WARNINGS     // Windows fix for being annoying.
+#define _CRT_SECURE_NO_WARNINGS                                                                                     // Windows fix for being annoying.
 #endif
 
 #ifdef BUILD_TYPE_VITA
-#define __psp2__                    // Vita fix for being slightly annoying.
+#define __psp2__                                                                                                    // Vita fix for being slightly annoying.
+int _newlib_heap_size_user   = #ifdef VITA_NEWLIB_HEAP_SIZE VITA_NEWLIB_HEAP_SIZE #else 100 * 1024 * 1024 #endif;   // 100MB default VITASDK heap, should be more than enough.
+unsigned int sceLibcHeapSize = #ifdef VITA_SCELIBC_HEAP_SIZE VITA_SCELIBC_HEAP_SIZE #else 10 * 1024 * 1024 #endif;  // 10MB default SCELibC heap, should be enough.
 #endif
 
 #include "std/error.h"
